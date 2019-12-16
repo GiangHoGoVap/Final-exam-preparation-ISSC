87f923e55cd6f09a336854de6737397c38e9a083
GiangHoGoVap <58210406+GiangHoGoVap@users.noreply.github.com>
2019-12-16 08:01:59 +0700

    Add files via upload

---
+++ problem9/problem9.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+int main(){
+	int x;
+	ifstream in_stream;
+	ofstream out_stream;
+	
+	in_stream.open("myfile.txt");
+	in_stream >> x;
+	out_stream.open("myfile.txt");
+	
+	cout << "The number read is: " << x << endl;
+	return 0;
+}
